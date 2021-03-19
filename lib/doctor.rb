e8d5819b78412b8dd929edd975cdf082a7a63903
Che <cgoranov@gmail.com>
2021-03-19 11:34:36 -0500

    commit tree 10412667e16d8c7d1e26119a3e7a1104b0c94edd

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
 
+require 'pry'
+
+class Doctor
+
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| appointment.doctor == self}
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments = Appointment.all.select {|appointment| appointment.doctor == self}
+        appointments.map {|x| x.patient}
+    end
+
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class Patient
+
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self}
+    end
+
+    def doctors
+        appointments = Appointment.all.select {|appointment| appointment.patient == self}
+        appointments.map {|x| x.doctor}
+    end
+
+
+end
